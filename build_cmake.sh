c699ce58a96ea0a567b16d00c95efb2c1a491d5c
rgbforge <155690697+rgbforge@users.noreply.github.com>
2025-09-10 14:39:09 -0500

    Create build_cmake.sh

---
+++ build_cmake.sh
@@ -0,0 +1,15 @@
+BUILDPATH=$(pwd)
+
+NCPUS=XX
+
+mkdir $BUILDPATH/CMAKE
+mkdir $BUILDPATH/CMAKE/install
+cd $BUILDPATH/CMAKE
+
+wget https://github.com/Kitware/CMake/archive/refs/tags/v3.30.9.tar.gz
+tar xzf v3.30.9.tar.gz
+
+cd CMake-3.30.9
+
+CC=gcc CXX=g++ ./configure --prefix=$BUILDPATH/CMAKE/install --parallel=$NCPUS
+
